a7cfac5bb9ea56da012f80bbbfaa6f966184ec8b
Sijia Wang <sijiaoh@outlook.com>
2020-11-15 17:04:15 +0900

    Add express hello world.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,9 @@
-console.log('Try npm run lint/fix!');
+import express from 'express';
 
-const longString =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+const app = express();
 
-const trailing = 'Semicolon';
+app.get('/', (_, res) => {
+  res.send('hello world');
+});
 
-const why = 'am I tabbed?';
-
-export function doSomeStuff(
-  withThis: string,
-  andThat: string,
-  andThose: string[]
-) {
-  //function on one line
-  if (!andThose.length) {
-    return false;
-  }
-  console.log(withThis);
-  console.log(andThat);
-  console.dir(andThose);
-  return;
-}
-// TODO: more examples
+app.listen(8080);
