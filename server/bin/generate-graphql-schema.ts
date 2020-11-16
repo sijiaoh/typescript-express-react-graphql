778bab79215b14d2eb92023d54ab8ee6f71eeefa
Sijia Wang <sijiaoh@outlook.com>
2020-11-17 02:32:07 +0900

    Move schema.gql to server directory.

--- server/bin/generate-graphql-schema.ts
+++ server/bin/generate-graphql-schema.ts
@@ -3,5 +3,4 @@ import 'reflect-metadata';
 import path from 'path';
 import {buildSchema} from '../src/graphql/buildSchema';
 
-const projectRootPath = path.dirname(path.dirname(__dirname))
-buildSchema({emitSchemaFile: `${projectRootPath}/client/generated/schema.gql`});
+buildSchema({emitSchemaFile: `${path.dirname(__dirname)}/generated/schema.gql`});
